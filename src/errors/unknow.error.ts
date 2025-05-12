254c13e1e3ff9a61a18cbf3f82334c8398b46586
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-05-12 11:30:29 -0300

    created base errors class

---
+++ src/errors/unknow.error.ts
@@ -0,0 +1,13 @@
+export class UnknownError extends Error {
+  constructor(error?: unknown) {
+    super(
+      error instanceof Error
+        ? error.message
+        : typeof error === "string"
+        ? error
+        : "Erro desconhecido no servidor!"
+    );
+    this.name = this.constructor.name;
+    Error.captureStackTrace(this, this.constructor);
+  }
+}
