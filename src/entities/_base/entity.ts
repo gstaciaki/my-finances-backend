b0b4fc6f3bb1ec7e627b2d6d455bf8574209f4a6
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-05-12 11:06:57 -0300

    created base entity class

---
+++ src/entities/_base/entity.ts
@@ -0,0 +1,23 @@
+import { v4 as uuid } from "uuid";
+
+export interface BaseProps {
+  id?: string;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export class BaseEntity {
+  public readonly id: string;
+  public readonly createdAt: Date;
+  public updatedAt: Date;
+
+  constructor(props?: BaseProps) {
+    this.id = props?.id ?? uuid();
+    this.createdAt = props?.createdAt ?? new Date();
+    this.updatedAt = props?.updatedAt ?? new Date();
+  }
+
+  touch() {
+    this.updatedAt = new Date();
+  }
+}
