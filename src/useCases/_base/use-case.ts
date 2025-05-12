f44d91bd95cd8e431e4a354d1ae29cb016b8615b
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-05-12 11:36:13 -0300

    created abstract class use case

---
+++ src/useCases/_base/use-case.ts
@@ -0,0 +1,36 @@
+import { InputValidationError } from "@src/errors/input-validation.error";
+import { UnknownError } from "@src/errors/unknow.error";
+import { Either, wrong } from "@src/util/either";
+
+export abstract class AbstractUseCase<
+  Input,
+  FailOutput extends Error,
+  SuccessOutput
+> {
+  constructor() {}
+
+  protected abstract execute(
+    input?: Input
+  ): Promise<
+    Either<FailOutput | InputValidationError | UnknownError, SuccessOutput>
+  >;
+
+  protected abstract validate(input: Input): Either<InputValidationError, void>;
+
+  public async run(
+    input: Input
+  ): Promise<
+    Either<FailOutput | InputValidationError | UnknownError, SuccessOutput>
+  > {
+    try {
+      const validation = this.validate(input);
+      if (validation.isWrong()) {
+        return wrong(validation.value);
+      }
+      const result = await this.execute(input);
+      return result;
+    } catch (error) {
+      return wrong(new UnknownError(error));
+    }
+  }
+}
