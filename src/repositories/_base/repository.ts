911f46b158adf96115799e38ca93730350d72f63
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-05-12 11:07:12 -0300

    created base repository class

---
+++ src/repositories/_base/repository.ts
@@ -0,0 +1,31 @@
+export interface IBaseRepository<T> {
+  create(data: T): Promise<T>;
+  findById(id: string): Promise<T | null>;
+  findAll(): Promise<T[]>;
+  update(id: string, data: Partial<T>): Promise<T>;
+  delete(id: string): Promise<void>;
+}
+
+export abstract class BaseRepository<T> implements IBaseRepository<T> {
+  protected abstract model: any;
+
+  async create(data: T): Promise<T> {
+    return this.model.create({ data });
+  }
+
+  async findById(id: string): Promise<T | null> {
+    return this.model.findUnique({ where: { id } });
+  }
+
+  async findAll(): Promise<T[]> {
+    return this.model.findMany();
+  }
+
+  async update(id: string, data: Partial<T>): Promise<T> {
+    return this.model.update({ where: { id }, data });
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.model.delete({ where: { id } });
+  }
+}
