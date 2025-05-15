c69786c11dbb83889739709a0648c7a0d4be26ec
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-05-15 00:13:13 -0300

    improve db test reset

--- test/helpers/base-test.ts
+++ test/helpers/base-test.ts
@@ -1,9 +1,27 @@
 import { execSync } from 'child_process';
-import { beforeEach } from '@jest/globals';
+import { beforeAll, beforeEach } from '@jest/globals';
+import { prisma } from '@src/database';
 
 export function setupDatabaseLifecycle() {
-  beforeEach(() => {
-    console.log('[setup] Executando antes de cada teste...');
+  beforeAll(() => {
+    console.log('[setup] Executando antes de todos os testes...');
     execSync('yarn test:prepare', { stdio: 'inherit' });
   });
+  beforeEach(async () => {
+    await cleanDatabase();
+  });
+}
+
+export async function cleanDatabase() {
+  const modelKeys = Object.keys(prisma).filter(
+    key =>
+      typeof prisma[key as keyof typeof prisma] === 'object' &&
+      // @ts-ignore
+      typeof prisma[key as keyof typeof prisma]?.deleteMany === 'function',
+  );
+
+  for (const model of modelKeys) {
+    // @ts-ignore
+    await prisma[model].deleteMany();
+  }
 }
