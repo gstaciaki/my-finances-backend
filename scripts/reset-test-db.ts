f37f2b3a88db6b45cb4d29d6851c7742547eb7dc
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-05-21 02:03:44 -0300

    help

--- scripts/reset-test-db.ts
+++ scripts/reset-test-db.ts
@@ -39,7 +39,7 @@ async function ensureDatabaseExists() {
 
 async function resetDatabase() {
   console.log('[debug] DATABASE_URL_TEST =', process.env.DATABASE_URL_TEST);
-  await waitForDatabase();
+  // await waitForDatabase();
 
   await ensureDatabaseExists();
 
