69e8608ffb4f41d70bcbba3e4043c256125fe80b
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-05-21 00:49:11 -0300

    refactor user prefab

--- test/prefab/user.ts
+++ test/prefab/user.ts
@@ -2,11 +2,27 @@ import { User } from '@src/entities/user.entity';
 import { cpf } from './cpf';
 import { faker } from '@faker-js/faker/.';
 
-export const genUser = (): User =>
+type UserInput = {
+  name?: string;
+  email?: string;
+  password?: string;
+  userCpf?: string;
+};
+
+export const genPassword = (): string => {
+  return faker.internet.password();
+};
+
+export const genUser = ({
+  name = faker.person.fullName(),
+  email = faker.internet.email(),
+  password = genPassword(),
+  userCpf = cpf(),
+}: UserInput = {}): User =>
   new User({
     id: faker.string.uuid(),
-    name: faker.person.fullName(),
-    email: faker.internet.email(),
-    password: faker.internet.password(),
-    cpf: cpf(),
+    name,
+    email,
+    password,
+    cpf: userCpf,
   });
