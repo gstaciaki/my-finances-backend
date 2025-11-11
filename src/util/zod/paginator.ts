301b12e0254275854560437c23a6d1b9bc13cb86
Guilherme Staciaki <gstaciaki99@gmail.com>
2025-11-11 19:42:50 -0300

    refactor(pagination): enhance pagiation types and base input

--- src/util/zod/paginator.ts
+++ src/util/zod/paginator.ts
@@ -1,8 +1,10 @@
 import { z } from 'zod';
 
 export const BasePaginatorSchema = z.object({
-  page: z.coerce.number().optional(),
-  limit: z.coerce.number().optional(),
+  page: z.coerce.number().min(1).default(1),
+  limit: z.coerce.number().min(1).max(100).default(10),
   createdAt: z.coerce.date().optional(),
   updatedAt: z.coerce.date().optional(),
 });
+
+export type BasePaginatorInput = z.infer<typeof BasePaginatorSchema>;
